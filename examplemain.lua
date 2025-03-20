e29d55a0bb9ae61e828bb6fcafa617894a49e540
Gandshack <32725188+Gandshack@users.noreply.github.com>
2025-03-19 23:05:15 -0500

    Add files via upload

--- examplemain.lua
+++ examplemain.lua
@@ -1,5 +1,5 @@
 print("Example App Main File")
-os.loadAPI("exampleapp/dependencies/exampledepenancy.lua
+os.loadAPI("exampleapp/dependencies/exampledepenancy.lua")
 os.loadAPI("exampleapp/dependencies/examplelayer/exampledepenecylayered.lua")
 
 exampledepenancy.printMessage()
